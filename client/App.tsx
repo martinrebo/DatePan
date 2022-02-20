96d6920dda5740df990b3000e07213b964589031
martin.reboredo@veepee.com <martin.reboredo@veepee.com>
2022-02-20 11:44:24 +0100

    :sparkles: New Landing - Under Construction - Page

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+import { Landing } from './screens/Landing';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +18,12 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        {/* <Navigation colorScheme={colorScheme} />
+        <StatusBar /> */}
+        <ThemeProvider>
+        <Landing />
+        </ThemeProvider>
+      
       </SafeAreaProvider>
     );
   }
