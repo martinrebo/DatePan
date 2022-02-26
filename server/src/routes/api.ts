4b5aa65b60933eaa7fb5291ffa8d003807fc1d03
martin.reboredo@veepee.com <martin.reboredo@veepee.com>
2022-02-26 12:52:33 +0100

    ✨  Add /api/system/ping

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -3,12 +3,13 @@ import { authMw } from './middleware';
 import authRouter from './auth-router';
 import userRouter from './user-router';
 import chatRouter from './chat-router';
-
+import sysRouter  from './system';
 
 // Init
 const apiRouter = Router();
 
 // Add api routes
+apiRouter.use('/system', sysRouter)
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', authMw, userRouter);
 apiRouter.use('/chat', authMw, chatRouter)
